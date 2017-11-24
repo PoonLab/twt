2725a722d6a878cb371450eaa6411e935f30d620
Tammy Ng <tng92@uwo.ca>
2017-11-24 11:14:12 -0500

    Modified Lineages parsing in Compartment objects to correctly store samplingTimes and parents --Tammy Ng

--- loadCompartments.R
+++ loadCompartments.R
@@ -20,14 +20,17 @@ load.compartments <- function(file) {
                              migration.rates = params$migration.rates
                              )
   })
+  # invisible(list2env(init.objects, .GlobalEnv))               # Should the objects be stored in a list or as indiividual objects accessed in the global environment?
   
   init.compartments <- sapply(compartments, function(x) {
     params <- settings$Compartments[[x]]
     x <- Compartment$new(type = params$type,
-                         lineages = params$lineages            # TODO: samplingTimes and parents not stored properly
+                         lineages = lapply(params$lineages, function(x) names(x) <- x)
                          )
   })
+  # invisible(list2env(init.compartments, .GlobalEnv))
   
+  return (c(init.objects, init.compartments))
 }
 
 
@@ -36,39 +39,36 @@ load.compartments <- function(file) {
 
 
 #--------------------------------------------------------------------------------------------
+## Are these getters and setters needed? They can be easily accessed directly within each object
 
 # retrieve a transmission rate for a given Compartment object
 get.transmission.rate <- function(CompartmentType, Compartment) {
-  if (!is.element(Compartment, names(CompartmentType$transmission.rates))) {
-    stop('Compartment "', Compartment, '" is not found')
-  }
+  if (!exists(CompartmentType)) { stop('CompartmentType "', CompartmentType, '" is not found') }     #could also check if present specifically in the Global env
+  if (!is.element(Compartment, names(CompartmentType$transmission.rates))) { stop('Compartment "', Compartment, '" is not found') }
   index <- which(names(CompartmentType$transmission.rates) == Compartment)
   return(CompartmentType$transmission.rates[index])
 }
 
 # set a new transmission rate for a given Compartment object 
 set.transmission.rate <- function(CompartmentType, Compartment, new.transmission.rate) {
-  if (!is.element(Compartment, names(CompartmentType$transmission.rates))) {
-    stop('Compartment "', Compartment, '" is not found')
-  }
+  if (!exists(CompartmentType)) { stop('CompartmentType "', CompartmentType, '" is not found') }     
+  if (!is.element(Compartment, names(CompartmentType$transmission.rates))) { stop('Compartment "', Compartment, '" is not found') }
   index <- which(names(CompartmentType$transmission.rates) == Compartment)
   CompartmentType$transmission.rates[index] <- new.transmission.rate
 }
 
 # retrieve a migration rate for a given Compartment object
 get.migration.rate <- function(CompartmentType, Compartment) {
-  if (!is.element(Compartment, names(CompartmentType$migration.rates))) {
-    stop('Compartment "', Compartment, '" is not found')
-  }
+  if (!exists(CompartmentType)) { stop('CompartmentType "', CompartmentType, '" is not found') }
+  if (!is.element(Compartment, names(CompartmentType$migration.rates))) { stop('Compartment "', Compartment, '" is not found') }
   index <- which(names(CompartmentType$migration.rates) == Compartment)
   return(CompartmentType$migration.rates[index])
 }
 
 # set a new migration rate for a given Compartment object
 set.migration.rate <- function(CompartmentType, Compartment, new.migration.rate) {
-  if(!is.element(Compartment, names(CompartmentType$migration.rates))) {
-    stop('Compartment "', Compartment, '" is not found')
-  }
+  if (!exists(CompartmentType)) { stop('CompartmentType "', CompartmentType, '" is not found') }
+  if (!is.element(Compartment, names(CompartmentType$migration.rates))) { stop('Compartment "', Compartment, '" is not found') }
   index <- which(names(CompartmentType$migration.rates) == Compartment)
-  CompartmentType$migration.rates[index] <- new.migration.rate
+  CompartmentType$migration.rates[index] <- as.numeric(new.migration.rate)
 }
