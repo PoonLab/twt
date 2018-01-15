f462babdd0f35c6af868dd9ee568bc2b8d215398
Tammy Ng <tng92@uwo.ca>
2018-01-15 12:42:38 -0500

    Working on populating empty tree with fixed lineage sampling times --Tammy Ng

--- initializeFixedEvents.R
+++ initializeFixedEvents.R
@@ -2,16 +2,20 @@
 # after the objects are generated from user inputs, we need to initialize the list of fixed events
 
 # retrieve sampling time and populate tip labels / times in ape::phylo object (building it tips up)
-init.fixed.samplings <- function(inputs) {
+init.fixed.samplings <- function(inputs, new.tree) {
   # add lineage sampling events from Lineage objects
   lineages <- inputs$get.lineages()
-  samplings <- lapply(lineages, function(x) {
-    indiv.sampled <- c()
-    sampling.times <- x$get.sampling.time()     # could either be a single sampling time or a vector of multiple sampling times
-    res <- sapply(sampling.times, function(y) {
-      indiv.sampled <- rbind(indiv.sampled, c(y, x$get.name()))
+  ind <- 1
+  samplings <- sapply(lineages, function(x) {
+    sapply(x, function(y) {
+      label <- y$get.name()
+      tip.height <- y$get.sampling.time()
+      # create label with corresponding tip height in new ape::phylo object, and populate edge matrix w/ initial values
+      new.tree$tip.label[ind] <- label
+      new.tree$edge.length[ind] <- tip.height
+      new.tree$edge[ind,2] <- ind
+      new.tree
     })
-    t(res)
   })
   samplings
 }
