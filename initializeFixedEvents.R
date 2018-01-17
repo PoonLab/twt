b635a4cc21e0f9734b64db8525df9ce87c26116d
Tammy Ng <tng92@uwo.ca>
2018-01-17 12:48:49 -0500

    Modified init.fixed.samplings to return vector of initialized tip labels and tip heights, and modified init.fixed.transmissions to populate EventLogger object --Tammy Ng

--- initializeFixedEvents.R
+++ initializeFixedEvents.R
@@ -2,39 +2,40 @@
 # after the objects are generated from user inputs, we need to initialize the list of fixed events
 
 # retrieve sampling time and populate tip labels / times in ape::phylo object (building it tips up)
-init.fixed.samplings <- function(inputs, new.tree) {
+init.fixed.samplings <- function(inputs) {
   # add lineage sampling events from Lineage objects
-  lineages <- inputs$get.lineages()
-  ind <- 1
-  samplings <- sapply(lineages, function(x) {
-    sapply(x, function(y) {
-      label <- y$get.name()
-      tip.height <- y$get.sampling.time()
-      # create label with corresponding tip height in new ape::phylo object, and populate edge matrix w/ initial values
-      new.tree$tip.label[ind] <- label
-      new.tree$edge.length[ind] <- tip.height
-      new.tree$edge[ind,2] <- ind
-      new.tree
-    })
-  })
-  samplings
+  lineages <- unlist(inputs$get.lineages())
+  tips.n.heights <- list(tip.label=character(), edge.length=numeric())
+  for (x in 1:length(lineages)) {
+    tip <- lineages[[x]]
+    label <- tip$get.name()
+    tip.height <- tip$get.sampling.time()
+    # store label w/ corresponding tip height in new ape::phylo object (not casted into `phylo` yet)
+    tips.n.heights$tip.label[x] <- label
+    tips.n.heights$edge.length[x] <- tip.height
+    tips.n.heights
+  }
+  tips.n.heights
 }
 
 
-init.fixed.transmissions <- function(inputs) {
+init.fixed.transmissions <- function(inputs, eventlog) {
   # if the user input includes a tree (host tree) then add transmission events
-  comps <- inputs$get.compartments()
-  fixed.events <- c()
+  comps <- unlist(inputs$get.compartments())
+  lineages <- unlist(inputs$get.lineages())
+
   transmissions <- sapply(comps, function(x) {
+    inf.time <- x$get.inf.time()
+      
     if (is.R6(x$get.source())) {
       source <- x$get.source()$get.name()
+      lineage <- lineages[[ which( sapply(lineages, function(y){which(y$get.location()$get.name() == x$get.name())}) == 1) ]]$get.name()
     } else {
       source <- x$get.source()
+      lineage <- NA
     }
-    inf.time <- x$get.inf.time()
-    # add row in format c(inf.time, source, x)
-    fixed.events <- rbind(fixed.events, c(inf.time, source, x$get.name()))
+
+    # add transmission event to EventLogger object
+    eventlog$add.event('transmission',  inf.time, lineage, x$get.name(), source)
   })
-  t(transmissions)
-  
 }
