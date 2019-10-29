488d05870167d3ed6a2d407eb58fc787069d235f
ArtPoon <artpoon@gmail.com>
2019-10-29 15:16:08 -0400

    revising simMigrations.R to handle new classes

--- pkg/R/simMigrations.R
+++ pkg/R/simMigrations.R
@@ -20,10 +20,10 @@
 #' @examples 
 #' path <- system.file('extdata', 'structSI.yaml', package='twt')
 #' settings <- yaml.load_file(path)
-#' mod <- MODEL$new(settings)
+#' mod <- Model$new(settings)
 #' 
-#' tree <- sim.outer.tree(mod)
-#' sim.migrations(mod, tree)
+#' run <- sim.outer.tree(mod)
+#' sim.migrations(run)
 #' tree$get.migration.events()  # display private data frame
 #' tree2 <- sim.inner.tree(mod, tree)
 #' 
@@ -35,27 +35,37 @@ sim.migrations <- function(run) {
   
   # unpack Run object
   comps <- run$get.compartments()
+  lineages <- run$get.lineages()
   types <- run$get.types()
+  eventlog <- run$get.eventlog()
+  
+  # CompartmentType name for each sampled compartment
   indiv.types <- sapply(unlist(comps), function(a) {a$get.type()$get.name()})
   
   # record population totals and migration rates for all Types
-  popn.totals <- run$get.origin.times()
+  init.conds <- run$get.initial.conds()
+  popn.totals <- init.conds$size
   popn.migration.rates <- .record.migration.rates(types, indiv.types)
-
-  # record max sampling time of lineages (the time of the most recent sample)
-  max.sampling.time <- min(sapply(model$get.lineages(), function(x) {x$get.sampling.time()}))
   
-  # record possible source types for each Compartment type
-  possible.source.types <- .record.possible.source.types(types, model$get.lineages())
-  
-  # extract transmission events that occurred from `sim.outer.tree()`
-  transmission.events <- eventlog$get.events('transmission')
-  
-  # generate migration events based on population dynamics up to the max sampling time
-  m_events <- .calc.migration.events(popn.totals, popn.migration.rates, max.sampling.time, possible.source.types, transmission.events)
-  
-  # store generated migration events into EventLogger, to be resolved later in `sim.inner.tree()`
-  eventlog$store.migration.events(m_events)
+  if (any(popn.migration.rates > 0)) {
+    # record max sampling time of lineages (the time of the most recent sample)
+    max.sampling.time <- min(sapply(lineages, function(x) { x$get.sampling.time() }))
+    
+    # record possible source types for each Compartment type
+    possible.source.types <- .record.possible.source.types(types, lineages)
+    
+    # extract transmission events that occurred from `sim.outer.tree()`
+    transmission.events <- eventlog$get.events('transmission')
+    
+    # generate migration events based on population dynamics up to the max sampling time
+    m_events <- .calc.migration.events(
+      popn.totals, popn.migration.rates, max.sampling.time, possible.source.types, 
+      transmission.events
+      )
+    
+    # store generated migration events into EventLogger, to be resolved later in `sim.inner.tree()`
+    eventlog$store.migration.events(m_events)  
+  }
 }
 
 
@@ -78,6 +88,10 @@ sim.migrations <- function(run) {
   for (x in types) {
     for (y in names(types)) {
       mig.rate <- x$get.migration.rate(y)
+      if (is.null(mig.rate)) {
+        # assume missing rate is zero
+        mig.rate <- 0
+      }
       popn.migration.rates[x$get.name(), y] <- mig.rate
     }
   }
@@ -110,69 +124,67 @@ sim.migrations <- function(run) {
 #' 
 #' Generates migration events only, based on population dynamics of the MODEL.
 #'
-#' @param popn.totals: totals at time `t=0` of susceptible and infected specific 
-#' to each CompartmentType
+#' @param init.conds: initial conditions specified in model settings, i.e., 
+#'        originTime and size (per CompartmentType)
 #' @param popn.migration.rates: rates of migration between different 
-#' CompartmentTypes
+#'        CompartmentTypes
 #' @param max.sampling.time: time of the most recent sample as a endpoint for 
-#' this function (type numeric)
+#'        this function (type numeric)
 #' @param possible.source.types: list of possible Sources that each recipient 
-#' type can receive a migrating lineage from
-#' @return m_events = data frame of migration events, each made up of: time, 
-#' recipient Type, and source Type
+#'        type can receive a migrating lineage from
+#'        
+#' @return data frame of migration events, each made up of: time, 
+#'         recipient Type, and source Type
 #' @keywords internal
-.calc.migration.events <- function(popn.totals, popn.migration.rates, 
-                                   max.sampling.time, possible.source.types, 
-                                   transmission.events) {
-  
+.calc.migration.events <- function(init.conds, popn.migration.rates, 
+                                   max.sampling.time, transmission.events) {
+  # prepare outcome container
   m_events <- data.frame(time=numeric(), r_type=character(), s_type=character(), 
                          v_type=character(), stringsAsFactors = FALSE)
   
-  for (v in 1:nrow(popn.totals)) {
+  total.outrate <- apply(popn.migration.rates, 1, sum)
+  r.types <- colnames(popn.migration.rates)[total.outrate > 0]
+
+  current.time <- init.conds$originTime
+  starting.infection <- 1
+  
+  # check if most recent sample is earlier than the epidemic start time
+  if (max.sampling.time > current.time) {
+    stop ('Not possible to have Lineage sampling time(s) precede the start time of the "', v.name, '"epidemic. Please set the start time of the epidemic further back in time.')
+  }
+  
+  for (i in transmission.events$time) {
     
-    v.name <- rownames(popn.totals)[v]
-    virus <- popn.totals[v,]
-    r.types <- names(possible.source.types)
-    current.time <- as.numeric(virus['start'])     # current time starts at user given time for each epidemic
-    starting.infection <- 1                              # starting infection
-    
-    # check if most recent sample is earlier than the epidemic start time
-    if (max.sampling.time > current.time) {
-      stop ('Not possible to have Lineage sampling time(s) precede the start time of the "', v.name, '"epidemic. Please set the start time of the epidemic further back in time.')
-    }
-    
-    for (i in transmission.events$time) {
+    temp.time <- i
+    while (temp.time > max.sampling.time) {
       
-      temp.time <- i
-      while (temp.time > max.sampling.time) {
-        
-        # sample source and recipient types for this generated migration evnet
-        r <- sample(r.types, 1)
-        s <- sample(possible.source.types[[r]], 1)
-        
-        # calculate waiting time until the next migration event
-        num.transmissions.occurred <- length(which(transmission.events$time >= temp.time))
-        rate <- popn.migration.rates[s,r] * (num.transmissions.occurred + starting.infection - 1)    # substract by 1, can't receive a migration from itself
-        if (rate <= 0) {
-          break
-        } else {
-          delta.t <- rexp(n=1, rate=rate)
-          migration.time <- temp.time - delta.t
-        }
-        
-        if (migration.time < max.sampling.time) { 
-          break
-        } else {
-          # store time, source and recipient types of migration event, and virus type
-          m_events <- rbind(m_events, list(time=migration.time, r_type=r, s_type=s, v_type=v.name), stringsAsFactors=F)
-          temp.time <- migration.time
-        }
-        
+      # sample source and recipient types for this generated migration evnet
+      r <- sample(r.types, 1)
+      s <- sample(possible.source.types[[r]], 1)
+      
+      # calculate waiting time until the next migration event
+      num.transmissions.occurred <- length(which(transmission.events$time >= temp.time))
+      rate <- popn.migration.rates[s,r] * (num.transmissions.occurred + starting.infection - 1)    # substract by 1, can't receive a migration from itself
+      if (rate <= 0) {
+        break
+      } else {
+        delta.t <- rexp(n=1, rate=rate)
+        migration.time <- temp.time - delta.t
+      }
+      
+      if (migration.time < max.sampling.time) { 
+        break
+      } else {
+        # store time, source and recipient types of migration event, and virus type
+        m_events <- rbind(m_events, list(time=migration.time, r_type=r, s_type=s, v_type=v.name), stringsAsFactors=F)
+        temp.time <- migration.time
       }
       
     }
     
   }
+    
+  
   
   m_events
 } 
