b9e645615ce9d705c8f8363fe5585a7b4795c929
Art Poon <artpoon@gmail.com>
2026-01-24 12:28:54 -0500

    refactored Model

--- R/Model.R
+++ R/Model.R
@@ -20,42 +20,28 @@ Model <- R6Class("Model",
   #lock_objects = FALSE,  # FIXME: is this deprecated?
   
   public = list(
-    initialize = function(settings=NA, name=NA) {
-      private$name <- name
-      private$parameters(settings)
+    initialize = function(settings=NA) {
+      private$load.parameters(settings)
       private$load.compartments(settings)
-      private$load.pathogens(settings)
-      private$fixed.samplings <- private$init.fixed.samplings()
+      private$load.sampling(settings)
     },
-    
-    # public variables
-    name = NULL,
     
     # ACCESSOR FUNCTIONS
     get.parameters = function() { private$parameters },
     get.compartments = function() { private$compartments },
-    get.hosts = function() { private$hosts },
-    get.pathogens = function() { private$pathogens },
     
     #' returns names of a given list of R6 objects
     #' @param listR6obj: list of R6 objects of class Compartment, Host or 
     #' Lineage
     get.names = function(listR6obj) {
       unname(sapply(listR6obj, function(x){ x$get.name() }))
-    },
-    
-    get.fixed.samplings = function() {
-      # retrieves fixed sampling times of tips
-      private$fixed.samplings
     }
   ),
   
   private = list(
     parameters = NULL,
     compartments = NULL,
-    hosts = NULL,
-    pathogens = NULL,
-    fixed.samplings = NULL,
+    sampling = NULL,
       
     load.parameters = function(settings) {
       if (is.null(settings$Parameters)) {
@@ -88,12 +74,12 @@ Model <- R6Class("Model",
         v <- eval(x, envir=env)
       }, error = function(e) {
         stop(paste(
-          "Expression `", x, "` contains one or more undeclared variables",
+          "Expression `", s, "` contains one or more undeclared variables"
         ))
       })
       if (!is.numeric(v)) {
         stop(paste(
-          "Expression `", x, "` does not evaluate to a numeric value."
+          "Expression `", s, "` does not evaluate to a numeric value"
         ))
       }
     },
@@ -121,28 +107,26 @@ Model <- R6Class("Model",
       for (cn in cnames) {
         eval(parse(text=paste(cn, "<-", 1)), envir=env)
       }
+      cat(eval(parse(text="ls()"), envir=env))
       
-      required <- c('rates', 'size')
       private$compartments <- sapply(cnames, function(cn) {
         params <- settings$Compartments[[cn]]
-        
-        # check that required parameters are present
-        missing <- which( !is.element(required, names(params)) )
-        if (length(missing) > 0) {
-          stop(paste("Compartment `", cn, "` missing required field(s): ", 
-                     required[missing]))
+        if (is.null(params$size)) {
+          stop("Compartment `", cn, "` must specify initial `size`")
         }
         
-        # check rate specifications
-        for (dest in names(params$rates)) {
-          rate <- params$rates[[dest]]  # from `cn` to `dest`
-          if (is.numeric(rate)) {
-            pass  # constant rate
-          } else if (is.character(rate)) {
-            check.expression(rate, env)
-          } else {
-            stop("Unexpected type in load.compartments")
-          }
+        if (!is.null(params$rates)) {
+          # check rate specifications
+          for (dest in names(params$rates)) {
+            rate <- params$rates[[dest]]  # from `cn` to `dest`
+            if (is.numeric(rate)) {
+              pass  # constant rate
+            } else if (is.character(rate)) {
+              private$check.expression(rate, env)
+            } else {
+              stop("Unexpected type in load.compartments")
+            }
+          }          
         }
         
         # check bottleneck setting
@@ -150,7 +134,7 @@ Model <- R6Class("Model",
         if (!is.null(params$bottleneck.size)) {
           bottleneck.size <- params$bottleneck.size
           if (!is.numeric(bottleneck.size)) {
-            check.expression(bottleneck.size, env)
+            private$check.expression(bottleneck.size, env)
           }
         }
         
@@ -159,7 +143,7 @@ Model <- R6Class("Model",
         if (!is.null(params$coalescent.rate)) {
           coalescent.rate <- params$coalescent.rate
           if (!is.numeric(coalescent.rate)) {
-            check.expression(coalescent.rate, env)
+            private$check.expression(coalescent.rate, env)
           }
         }
         
@@ -173,216 +157,43 @@ Model <- R6Class("Model",
       })
     },
     
-
-    load.pathogens = function(settings) {
-      if (is.null(settings$Pathogens)) {
-        stop("'Pathogens' is a required field in settings.")
+    # Validate Sampling settings
+    load.sampling = function(settings) {
+      if (is.null(settings$Sampling)) {
+        stop("'Sampling' is a required field in settings (YAML)")
       }
-      
-      # check pre-requisites
-      if (is.null(private$parameters)) {
-        load.parameters(settings)
+      private$sampling <- settings$Sampling
+      if (is.null(private$sampling$mode)) {
+        stop("'mode' is a required field within 'Sampling' in settings")
       }
-      if (is.null(private$compartments)) {
-        load.compartments(settings)
-      }
-      
-      # return object will be concatenated vector of Lineage objects
-      result <- unlist(sapply(names(settings$Lineages), function(label) {
-        if (grepl("_", label)) {
-          stop("Error: underscore characters are reserved, please modify Lineage name",
-               label)
-        }
-         
-        params <- settings$Lineages[[label]]
-        if (is.null(params$location)) {
-          stop("Lineage", label, "missing required field 'location'")
+      mode <- private$sampling$mode
+    
+      if (mode == "compartment") {
+        # one or more Compartments represent sampled hosts, i.e., constant 
+        # sampling rate.  Populate Host objects during trajectory simulation.
+        
+        if (is.null(private$sampling$targets)) {
+          stop("Sampling field must specify `targets` (compartments)")
         }
         
-        
-        # find all Compartments that match the specified location
-        if (length(private$compartments) == 0) {
-          stop("Compartments must be parsed before Lineages.")
+        # check that targeted compartments have been defined by user
+        if (is.null(private$compartments)) {
+          load.compartments(settings)
         }
-        
-        # FIXME: this should work..
-        #compNames <- self$get.names(Compartment)
-        compNames <- sapply(private$compartments, function(n){n$get.name()})
-        
-        searchComps <- which(grepl(paste0("^", params$location, "_"), compNames))
-        if (length(searchComps) == 0) {
-          stop(params$location, ' of Lineage ', label, 
-               ' is not a specified Compartment object.\n',
-               'Available compartments:\n', 
-               paste(compNames))
-        }
-        
-        
-        if (is.null(params$replicates)) {
-          nIndiv <- 1
-        } else {
-          nIndiv <- params$replicates  
-        }
-        
-        
-        if (is.null(params$sampling.time)) {
-          stop("Lineage", label, "missing required field 'sampling.time'")
-        }
-        if (is.numeric(params$sampling.time)) {
-          sampleTimes <- rep.int(params$sampling.time, times=nIndiv)
-        } 
-        else {
-          vec <- unlist(strsplit(params$sampling.time, split='[`(`|,|`)`]'))
-          
-          # exclude empty strings
-          sampleTimes <- as.double(vec[nzchar(x=vec)])
-          if (any(is.na(sampleTimes))) {
-            # failed to parse string as double
-            stop('\nSampling times must all be specified as type `numeric`', 
-                 ' or type `double`. Sampling time "', vec, '" is of type `', 
-                 typeof(vec), '`.\n')
+        for (cn in names(private$sampling$targets)) {
+          if (!is.element(cn, names(private$compartments))) {
+            stop(paste("In Sampling:targets compartment", cn, 
+                       "has not been declared in Compartments"))
           }
-          if (length(sampleTimes) != nIndiv) {
-            stop('attribute `sampling.time` of Lineage ', label, 
-                 ' does not match number of replicates specified for',
-                 'respective Lineage.')
+          count <- private$sampling$targets[[cn]]
+          if (!is.integer(count) | count <= 0) {
+            stop(paste("Sampling:targets:", cn, "must be a positive integer"))
           }
         }
-
-                
-        sapply(searchComps, function(compIdx) {
-          comp <- private$compartments[[compIdx]]
-          
-          sapply(1:nIndiv, function(i) {            
-            # generate unique identifier
-            new.lineage <- Lineage$new(
-              # for example, "host_3__virus_2"
-              name = paste0(comp$get.name(),'__',label,'_',i),
-              type = params$type,  # LineageType, UNUSED
-              sampling.time = sampleTimes[i],
-              location = comp  # reference to Compartment object
-            )
-            # add Lineage to Compartment
-            # this also updates Compartment sampling.time attribute
-            comp$add.lineage(new.lineage)
-            
-            new.lineage  # return object
-          })
-        })
-      }))
-      
-      names(result) <- sapply(result, function(l) l$get.name())
-      result
-    },
-    
-    
-    
-    init.popn.growth.dynamics = function(pieces) {
-      # @param pieces, list of linear pieces of a given CompartmentType
-      # @return matrix, where each row represents a linear piece and comprises the following columns:
-        # time = inputted time of the piece
-        # popn = inputted popn size of the piece at time `time`
-        # slope = slope of the piece 
-        # intercept = y-intercept of the piece
-      
-      if (is.null(pieces)) {
-        return (NULL)
-      }
-      mat <- matrix(
-        nrow=length(pieces), ncol=5, 
-        dimnames=list(1:length(pieces), 
-                      c('startTime', 'startPopn', 'endTime', 'endPopn', 'slope'))
-        )
-      
-      for (x in seq_along(pieces)) {
-        vec <- unlist(pieces[[x]])  # a named vector
-        
-        if (!is.element('startTime', names(vec))) {
-          stop ('Parameter "startTime" not defined for piece "', names(pieces)[[x]], '".')
-        } else {
-          mat[x,1] <- vec['startTime']
-        }
-        
-        if (!is.element('startPopn', names(vec))) {
-          stop ('Parameter "startPopn" not defined for piece "', names(pieces)[[x]], '".')
-        } else {
-          mat[x,2] <- vec['startPopn']
-        }
-        
-        if (!is.element('endTime', names(vec))) {
-          # for the final piece with `inf` time, assumed that population stays constant from startPopn
-          mat[x,3] <- NA
-          mat[x,4] <- vec['startPopn']
-        } 
-        else {
-          mat[x,3] <- vec['endTime']
-          if (!is.element('endPopn', names(vec))) {
-            stop ('Parameter "endPopn" not defined for piece "', names(pieces)[[x]], '".')
-          } else {
-            mat[x,4] <- vec['endPopn']
-          }
-        }
-        
-      }
-      
-      # checks for the following:
-      
-      # only one piece must have a start time of zero
-      if ( length(which(mat[,'startTime'] == 0)) != 1 ) {
-        stop ('One and only one linear piece in the population growth ',
-              'dynamics functions must have a start time of zero (forward in time).')
-      }
-      if ( length(which(is.na(mat[,'endTime']))) != 1 ) {
-        stop ('One and only one linear piece in the population growth ', 
-              'dynamics functions must have an end time of infinity (forward in time). ',
-              'The rest of the pieces must have attr `endTime` specified for each piece.')
-      }
-      # all times must be of mode numeric
-      if ( is.numeric(mat[,'startTime']) == F || is.numeric(mat[,'endTime']) == F ) {
-        stop ('All linear pieces must have start and end times in mode `numeric`.')
-      }
-      # all times must be unique
-      if ( length(unique(mat[,'startTime'])) < length(mat[,'startTime']) ) {
-        stop ('All linear pieces must have unique start times.')
-      }
-      # start and end times of middle pieces must be continuous (with no gaps or overlaps)
-      if ( length(intersect(mat[,'startTime'], mat[,'endTime'])) != (nrow(mat)-1) ) {
-        stop ('There are time gaps and/or overlapping times within the population ', 
-              'growth dynamics pieces.')
-      }
-      
-      # order pieces sequentially based on times (in order to calculate slopes)
-      if (nrow(mat) == 1) {
-        mat <- as.matrix(t(mat[order(mat[,'startTime']), ]))
-        rownames(mat) <- c(1:length(nrow(mat)))
       } else {
-        mat <- mat[order(mat[,'startTime']), ]
+        stop(paste("Sampling mode `", mode, "` not recognized"))
       }
-      
-      for (x in 1:nrow(mat)) {
-        # calculate slope and intercept for each piece and populate the matrix
-        if (x == nrow(mat)) {
-          # it is assumed that the final piece will continue towards infinite time at the same population
-          # size as the final piece's, with a slope of 0
-          slope <- 0
-        } else {
-          slope <- (mat[x, 'startPopn'] - mat[x, 'endPopn']) / 
-            (mat[x, 'endTime'] - mat[x, 'startTime'])
-        }
-        #yInt <- mat[x, 'startPopn'] - slope * mat[x, 'startTime']
-        mat[x, 'slope'] <- slope
-        #mat[x, 'intercept'] <- yInt
-      }
-      mat
-    },
-    
-    
-    init.fixed.samplings = function() {
-      # retrieve sampling time and populate tip labels / times in ape:: phylo object 
-      # (for plotting Eventlogger function)
-      sapply(private$lineages, function(x) x$get.sampling.time())
     }
-    
   )
 )
 
@@ -391,25 +202,19 @@ Model <- R6Class("Model",
 #' S3 class function to display contents of a Model object
 #' @export
 print.Model <- function(obj) {
-  cat("twt Model", ifelse(is.na(obj$name), '<unnamed>', obj$name), "\n\n")
-  
-  cat("Initial conditions:\n")
-  init <- obj$get.initial.conds()
-  cat("  originTime: ", init$originTime, "\n")
-  
-  cat("  size:\n")
-  for (i in 1:length(init$size)) {
-    cat(paste0("    ", names(init$size)[i], ": ", init$size[i]), "\n")
+  cat("twt Model")
+  cat("Parameters:\n")
+  params <- obj$get.parameters()
+  for (key in names(params)) {
+    value <- params[[key]]
+    cat("  ", key, ": ", value, "\n")
   }
-  
-  cat("  indexType: ", init$indexType, "\n")
-  
-  cat("CompartmentTypes:\n")
-  types <- obj$get.types()
-  for (i in 1:length(types)) {
-    ct <- types[[i]]
-    cat( sprintf("  %10s ", c(names(types)[i])) )
+  cat("Compartments: ")
+  compartments <- obj$get.compartments()
+  for (cn in names(compartments)) {
+    cat(cn, " ")
   }
+  cat("\n")
 }
 
 #' summary.Model
@@ -418,4 +223,11 @@ print.Model <- function(obj) {
 #' @export
 summary.Model <- function(obj) {
   print(obj)
+}
+
+
+#' plot.Model
+#' Plot a graph summarizing compartments and rates
+plot.Model <- function(obj) {
+  # TODO: work in progress!
 }
