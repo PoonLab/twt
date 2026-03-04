3b07a8deca5fd4198c590ecfe56e3e7c721d71bc
ArtPoon <artpoon@gmail.com>
2026-03-03 23:10:36 -0500

    Started to write unit test for InnerTree .relabel.inner.events

--- tests/testthat/test_InnerTree.R
+++ tests/testthat/test_InnerTree.R
@@ -15,3 +15,21 @@ test_that("InnerTree constructor", {
   result <- inner$get.active()$count.type()
   expect_equal(result, 0)
 })
+
+
+test_that("Relabel inner events", {
+  events <- data.frame(
+    time=c(3, 2, 1, 0, 0),
+    event=c('sampling', 'sampling', 'transmission', 'coalescent', 'coalescent'),
+    from.comp=c("I", "I", "S", "I", "I"),
+    to.comp=c("I_samp", "I_samp", "I", NA, NA),
+    from.host=c("I_1", "I_2", "I_1", "I_1", "I_1"),
+    to.host=c(NA, NA, "I_2", NA, NA),
+    pathogen1=c("P_1", "P_2", "P_2", "P_3", "P_3"),
+    pathogen2=c(NA, NA, NA, NA, NA)
+  )
+  result <- .relabel.inner.events(events)
+  expected <- data.frame(
+    
+  )
+})
