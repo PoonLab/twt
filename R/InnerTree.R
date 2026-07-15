73cac96de6e96b1dcea535c733cbf2e0c3fe2823
WongDWai <dwong338@uwo.ca>
2026-07-15 12:27:21 -0400

    Remove unused set.log method from InnerTree

--- R/InnerTree.R
+++ R/InnerTree.R
@@ -62,7 +62,6 @@ InnerTree <- R6Class(
     
     # accessor functions
     get.log = function() { private$inner.log },
-    set.log = function(new.log) { private$inner.log <- new.log },
     get.model = function() { private$mod },
     get.prefix = function() { private$prefix },
     get.pindex = function() { private$p.index },
